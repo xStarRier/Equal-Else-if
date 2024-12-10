f7da81e62ec1b87d73539b560369602c04b08cde
xStarRier <stephanniequynh@gmail.com>
2024-12-10 14:47:08 +0100

    Add project files.

---
+++ Equal Else if/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Lag en metode som tar imot to tall og returnerer true dersom tallene er like. 
+
+
+int ifEqual;
+
+int equal1 = 1;
+int equal2 = 29;
+EqualFunc();
+void EqualFunc(){ 
+Console.WriteLine("Insert Number");
+int equal1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Insert Number");
+int equal2 = Convert.ToInt32(Console.ReadLine());
+   if (equal1 == equal2)
+    {
+        
+        Console.WriteLine("The numbers are equal");
+    }
+    else
+    {
+        
+        Console.WriteLine("The numbers are not equal");
+    } 
+
+}
+
